291504338cea9efb1b13b45030017a3c06a77961
Jonathan Kunstwald <jonathan.kunstwald@rwth-aachen.de>
2019-11-12 20:14:15 +0100

    Remove CommandList, simplify ring ctor

--- src/phantasm-renderer/backend/d3d12/CommandList.cc
+++ src/phantasm-renderer/backend/d3d12/CommandList.cc
@@ -4,32 +4,10 @@
 
 #include "BackendD3D12.hh"
 
-void pr::backend::d3d12::CommandList::setDescriptorHeap(D3D12_DESCRIPTOR_HEAP_TYPE type, ID3D12DescriptorHeap* heap)
-{
-    if (mDescriptorHeaps[type] != heap)
-    {
-        mDescriptorHeaps[type] = heap;
-        rebindDescriptorHeaps();
-    }
-}
-
-void pr::backend::d3d12::CommandList::close() { mCommandList->Close(); }
-
-void pr::backend::d3d12::CommandList::rebindDescriptorHeaps()
-{
-    cc::capped_vector<ID3D12DescriptorHeap*, D3D12_DESCRIPTOR_HEAP_TYPE_NUM_TYPES> packed_desc_heaps;
-
-    for (auto const& heap : mDescriptorHeaps)
-        if (heap)
-            packed_desc_heaps.push_back(heap);
-
-    mCommandList->SetDescriptorHeaps(UINT(packed_desc_heaps.size()), packed_desc_heaps.data());
-}
-
 void pr::backend::d3d12::CommandListRing::initialize(pr::backend::d3d12::BackendD3D12& backend,
                                                      unsigned num_allocators,
                                                      unsigned num_command_lists_per_allocator,
-                                                     const D3D12_COMMAND_QUEUE_DESC& queue_desc)
+                                                     D3D12_COMMAND_LIST_TYPE type)
 {
     mRing = cc::array<ring_entry>::defaulted(num_allocators);
 
@@ -37,13 +15,13 @@ void pr::backend::d3d12::CommandListRing::initialize(pr::backend::d3d12::Backend
     for (auto& ring_entry : mRing)
     {
         // Create the allocator
-        PR_D3D12_VERIFY(device.CreateCommandAllocator(queue_desc.Type, PR_COM_WRITE(ring_entry.command_allocator)));
+        PR_D3D12_VERIFY(device.CreateCommandAllocator(type, PR_COM_WRITE(ring_entry.command_allocator)));
 
         // Create the command lists
         ring_entry.command_lists = decltype(ring_entry.command_lists)::defaulted(num_command_lists_per_allocator);
         for (auto& list : ring_entry.command_lists)
         {
-            PR_D3D12_VERIFY(device.CreateCommandList(0, queue_desc.Type, ring_entry.command_allocator, nullptr, PR_COM_WRITE(list)));
+            PR_D3D12_VERIFY(device.CreateCommandList(0, type, ring_entry.command_allocator, nullptr, PR_COM_WRITE(list)));
             list->Close();
         }
 
--- src/phantasm-renderer/backend/d3d12/CommandList.hh
+++ src/phantasm-renderer/backend/d3d12/CommandList.hh
@@ -14,41 +14,12 @@ namespace pr::backend::d3d12
 {
 class BackendD3D12;
 
-/// wrapper for ID3D12CommandList, unused
-struct CommandList
-{
-    explicit CommandList(ID3D12GraphicsCommandList* list) : mType(D3D12_COMMAND_LIST_TYPE_DIRECT), mCommandList(list) { cc::fill(mDescriptorHeaps, nullptr); }
-
-    void setDescriptorHeap(D3D12_DESCRIPTOR_HEAP_TYPE type, ID3D12DescriptorHeap* heap);
-
-    void close();
-
-public:
-    [[nodiscard]] D3D12_COMMAND_LIST_TYPE const& getType() const { return mType; }
-
-    [[nodiscard]] ID3D12GraphicsCommandList& ref() const { return *mCommandList; }
-    [[nodiscard]] ID3D12GraphicsCommandList* ptr() const { return mCommandList; }
-
-private:
-    void rebindDescriptorHeaps();
-
-private:
-    /// The type of this command list
-    D3D12_COMMAND_LIST_TYPE mType;
-
-    /// Raw, non-owning pointer
-    ID3D12GraphicsCommandList* mCommandList;
-
-    /// Weak references to the descriptor heaps that are currently bound
-    cc::array<ID3D12DescriptorHeap*, D3D12_DESCRIPTOR_HEAP_TYPE_NUM_TYPES> mDescriptorHeaps;
-};
-
 /// Ring buffer of command allocators, each with N command lists
 /// Unsynchronized
 class CommandListRing
 {
 public:
-    void initialize(BackendD3D12& backend, unsigned num_allocators, unsigned num_command_lists_per_allocator, D3D12_COMMAND_QUEUE_DESC const& queue_desc);
+    void initialize(BackendD3D12& backend, unsigned num_allocators, unsigned num_command_lists_per_allocator, D3D12_COMMAND_LIST_TYPE type);
 
     /// Steps forward in the ring, resetting the next allocator
     void onBeginFrame();
