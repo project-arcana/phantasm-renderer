1b292cad2c43cc501db584ab57255c578ace37fb
Jonathan Kunstwald <jonathan.kunstwald@rwth-aachen.de>
2019-11-12 18:39:50 +0100

    Remove old descriptor management code

--- src/phantasm-renderer/backend/d3d12/resources/resource_view.cc
+++ src/phantasm-renderer/backend/d3d12/resources/resource_view.cc
@@ -5,60 +5,6 @@
 #include <phantasm-renderer/backend/d3d12/common/d3d12_sanitized.hh>
 #include <phantasm-renderer/backend/d3d12/common/verify.hh>
 
-void pr::backend::d3d12::DescriptorHeap::initialize(ID3D12Device& device, D3D12_DESCRIPTOR_HEAP_TYPE heap_type, uint32_t num_descriptors)
-{
-    mNumDescriptors = num_descriptors;
-    mIndex = 0;
-    mDescriptorSize = device.GetDescriptorHandleIncrementSize(heap_type);
-
-    D3D12_DESCRIPTOR_HEAP_DESC descHeap;
-    descHeap.NumDescriptors = num_descriptors;
-    descHeap.Type = heap_type;
-    descHeap.Flags = ((heap_type == D3D12_DESCRIPTOR_HEAP_TYPE_RTV) || (heap_type == D3D12_DESCRIPTOR_HEAP_TYPE_DSV))
-                         ? D3D12_DESCRIPTOR_HEAP_FLAG_NONE
-                         : D3D12_DESCRIPTOR_HEAP_FLAG_SHADER_VISIBLE;
-    descHeap.NodeMask = 0;
-    PR_D3D12_VERIFY(device.CreateDescriptorHeap(&descHeap, PR_COM_WRITE(mHeap)));
-    mHeap->SetName(L"DescriptorHeap");
-}
-
-pr::backend::d3d12::resource_view pr::backend::d3d12::DescriptorHeap::allocDescriptor(uint32_t size)
-{
-    if ((mIndex + size) >= mNumDescriptors)
-    {
-        CC_RUNTIME_ASSERT(false && "StaticResourceViewHeap out of memory, increase its size");
-    }
-
-    D3D12_CPU_DESCRIPTOR_HANDLE cpu_view = mHeap->GetCPUDescriptorHandleForHeapStart();
-    cpu_view.ptr += mIndex * mDescriptorSize;
-
-    D3D12_GPU_DESCRIPTOR_HANDLE gpu_view = mHeap->GetGPUDescriptorHandleForHeapStart();
-    gpu_view.ptr += mIndex * mDescriptorSize;
-
-    mIndex += size;
-
-    return resource_view(size, mDescriptorSize, cpu_view, gpu_view);
-}
-
-void pr::backend::d3d12::DescriptorHeap::reset() { mIndex = 0; }
-
-void pr::backend::d3d12::ResourceViewAllocator::initialize(
-    ID3D12Device& device, uint32_t num_cbvs, uint32_t num_srvs, uint32_t num_uavs, uint32_t num_dsvs, uint32_t num_rtvs, uint32_t num_samplers)
-{
-    mHeapDSVs.initialize(device, D3D12_DESCRIPTOR_HEAP_TYPE_DSV, num_dsvs);
-    mHeapRTVs.initialize(device, D3D12_DESCRIPTOR_HEAP_TYPE_RTV, num_rtvs);
-    mHeapSamplers.initialize(device, D3D12_DESCRIPTOR_HEAP_TYPE_SAMPLER, num_samplers);
-    mHeapCBVsSRVsUAVs.initialize(device, D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV, num_cbvs + num_srvs + num_uavs);
-}
-
-void pr::backend::d3d12::ResourceViewAllocator::resetAll()
-{
-    mHeapDSVs.reset();
-    mHeapRTVs.reset();
-    mHeapSamplers.reset();
-    mHeapCBVsSRVsUAVs.reset();
-}
-
 void pr::backend::d3d12::StaticDescriptorPool::initialize(ID3D12Device& device, D3D12_DESCRIPTOR_HEAP_TYPE type, unsigned size)
 {
     auto const desc_size = device.GetDescriptorHandleIncrementSize(type);
--- src/phantasm-renderer/backend/d3d12/resources/resource_view.hh
+++ src/phantasm-renderer/backend/d3d12/resources/resource_view.hh
@@ -41,9 +41,9 @@ struct cpu_cbv_srv_uav : cpu_resource_view
 {
 };
 
-// this is NOT a static sampler, its a descriptor handle to a heap sampler
 struct cpu_sampler : cpu_resource_view
 {
+    // this is NOT a static sampler, its a descriptor handle to a heap sampler
 };
 
 struct cpu_dsv : cpu_resource_view
@@ -154,50 +154,5 @@ private:
 
     DynamicDescriptorRing mRingCBVsSRVsUAVs;
     DynamicDescriptorRing mRingSamplers;
-};
-
-class DescriptorHeap
-{
-public:
-    void initialize(ID3D12Device& pDevice, D3D12_DESCRIPTOR_HEAP_TYPE heapType, uint32_t descriptorCount);
-
-    /// Allocates a resource view of the given size
-    [[nodiscard]] resource_view allocDescriptor(uint32_t size);
-
-    /// Resets the underlying linear allocator, invalidating all previous resource views
-    void reset();
-
-    ID3D12DescriptorHeap* getHeap() const { return mHeap; }
-
-private:
-    shared_com_ptr<ID3D12DescriptorHeap> mHeap;
-    uint32_t mIndex = 0;
-    uint32_t mNumDescriptors = 0;
-    uint32_t mDescriptorSize = 0;
-};
-
-class ResourceViewAllocator
-{
-public:
-    void initialize(ID3D12Device& device, uint32_t num_cbvs, uint32_t num_srvs, uint32_t num_uavs, uint32_t num_dsvs, uint32_t num_rtvs, uint32_t num_samplers);
-
-    [[nodiscard]] resource_view allocCBV_SRV_UAV(uint32_t size) { return mHeapCBVsSRVsUAVs.allocDescriptor(size); }
-    [[nodiscard]] resource_view allocDSV(uint32_t size) { return mHeapDSVs.allocDescriptor(size); }
-    [[nodiscard]] resource_view allocRTV(uint32_t size) { return mHeapRTVs.allocDescriptor(size); }
-    [[nodiscard]] resource_view allocSampler(uint32_t size) { return mHeapSamplers.allocDescriptor(size); }
-
-    ID3D12DescriptorHeap* getDSVHeap() { return mHeapDSVs.getHeap(); }
-    ID3D12DescriptorHeap* getRTVHeap() { return mHeapRTVs.getHeap(); }
-    ID3D12DescriptorHeap* getSamplerHeap() { return mHeapSamplers.getHeap(); }
-    ID3D12DescriptorHeap* getCBV_SRV_UAVHeap() { return mHeapCBVsSRVsUAVs.getHeap(); }
-
-    /// Resets all underlying linear allocators, invalidating all previous resource views
-    void resetAll();
-
-private:
-    DescriptorHeap mHeapDSVs;
-    DescriptorHeap mHeapRTVs;
-    DescriptorHeap mHeapSamplers;
-    DescriptorHeap mHeapCBVsSRVsUAVs;
 };
 }
