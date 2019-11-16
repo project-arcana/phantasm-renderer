9cb238f93b77869b744fdc09244633016c1bf258
Jonathan Kunstwald <jonathan.kunstwald@rwth-aachen.de>
2019-11-17 00:27:51 +0100

    Use viewport flipping in Vulkan by default

--- src/phantasm-renderer/backend/vulkan/common/util.hh
+++ src/phantasm-renderer/backend/vulkan/common/util.hh
@@ -6,11 +6,16 @@ namespace pr::backend::vk::util
 {
 inline void set_viewport(VkCommandBuffer command_buf, VkExtent2D size)
 {
+    // This call sets a flipped viewport using the VK_KHR_Maintenance1 extension
+    // which is core in Vulkan 1.1 (which we use)
+    // this causes the viewport to behave as it does in OpenGL
+    // see https://www.saschawillems.de/blog/2019/03/29/flipping-the-vulkan-viewport/
+
     VkViewport viewport = {};
     viewport.x = 0.0f;
-    viewport.y = 0.0f;
+    viewport.y = float(size.height);
     viewport.width = float(size.width);
-    viewport.height = float(size.height);
+    viewport.height = -float(size.height);
     viewport.minDepth = 0.0f;
     viewport.maxDepth = 1.0f;
 
@@ -21,5 +26,4 @@ inline void set_viewport(VkCommandBuffer command_buf, VkExtent2D size)
     vkCmdSetViewport(command_buf, 0, 1, &viewport);
     vkCmdSetScissor(command_buf, 0, 1, &scissor);
 }
-
 }
