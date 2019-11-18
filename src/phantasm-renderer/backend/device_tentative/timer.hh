8a909f991eafe2253f5272cb762d04d51e2beb7f
Jonathan Kunstwald <jonathan.kunstwald@rwth-aachen.de>
2019-11-18 14:00:11 +0100

    Use chrono for device timer

--- src/phantasm-renderer/backend/device_tentative/timer.hh
+++ src/phantasm-renderer/backend/device_tentative/timer.hh
@@ -1,16 +1,32 @@
 #pragma once
 
+#include <chrono>
+
 namespace pr::backend::device
 {
 class Timer
 {
-    long long mResolution;
-    long long mStartTime;
+private:
+    std::chrono::time_point<std::chrono::high_resolution_clock> mStart;
 
 public:
-    Timer();
+    Timer() { restart(); }
 
-    void reset();
-    [[nodiscard]] double getElapsedTime() const;
+    /// Restart the timer
+    void restart() { mStart = std::chrono::high_resolution_clock::now(); }
+
+    /// Get the duration since last restart
+    float elapsedSeconds() const { return std::chrono::duration<float>(std::chrono::high_resolution_clock::now() - mStart).count(); }
+    double elapsedSecondsD() const { return std::chrono::duration<double>(std::chrono::high_resolution_clock::now() - mStart).count(); }
+
+    /// Get the duration since last restart in milliseconds
+    float elapsedMilliseconds() const { return std::chrono::duration<float, std::milli>(std::chrono::high_resolution_clock::now() - mStart).count(); }
+    double elapsedMillisecondsD() const
+    {
+        return std::chrono::duration<double, std::milli>(std::chrono::high_resolution_clock::now() - mStart).count();
+    }
+
+    /// Get the duration since last restart (integer, nanoseconds)
+    int64_t elapsedNanoseconds() const { return std::chrono::nanoseconds{std::chrono::high_resolution_clock::now() - mStart}.count(); }
 };
 }
