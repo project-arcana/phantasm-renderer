09c9dc0c38f91331e1261cb3746d0888e9d982fb
Jonathan Kunstwald <jonathan.kunstwald@rwth-aachen.de>
2019-11-21 00:33:44 +0100

    Add TODO comment in framebuffer cache

--- src/phantasm-renderer/backend/d3d12/pools/framebuffer_cache.hh
+++ src/phantasm-renderer/backend/d3d12/pools/framebuffer_cache.hh
@@ -10,25 +10,44 @@
 
 namespace pr::backend::d3d12
 {
-/// Persistent cache for root signatures
-/// Unsynchronized, only used inside of pso pool
-class RootSignatureCache
-{
-public:
-    using key_t = arg::shader_argument_shapes;
 
-    void initialize(unsigned size_estimate = 50);
-    void destroy();
+/// TODO, plan changed:
+/// there won't be an explicit handle::framebuffer,
+/// but it won't just be handle::resource for RTVs either
+///
+/// There are two places where "resource views" are API exposed:
+/// handle::shader_view
+///     (SRVs + UAVs for shaders)
+///
+/// TBD::SetRenderTargets bytecode within command list
+///     (RTVs + DSV for render targets)
+///
+/// However within this command, there's nothing but the handle::resource
+/// and the "how to view" info. We create small RTV + DSV heaps
+/// PER command list, which act as linear allocators and create the
+/// descriptors (remember, not GPU-visible) on the fly
+///
+/// The data structure for this linear allocator could be placed here,
+/// and must be referenced in CommandListPool::cmd_list_node
+///
 
-    /// receive an existing root signature matching the shape, or create a new one
-    /// returns a pointer (which remains stable, §23.2.5/13 C++11)
-    [[nodiscard]] root_signature_ll* getOrCreate(ID3D12Device& device, key_t arg_shapes);
+//class RootSignatureCache
+//{
+//public:
+//    using key_t = arg::shader_argument_shapes;
 
-    /// destroys all elements inside, and clears the map
-    void reset();
+//    void initialize(unsigned size_estimate = 50);
+//    void destroy();
 
-private:
-    std::unordered_map<key_t, root_signature_ll, hash::compute_functor> mCache;
-};
+//    /// receive an existing root signature matching the shape, or create a new one
+//    /// returns a pointer (which remains stable, §23.2.5/13 C++11)
+//    [[nodiscard]] root_signature_ll* getOrCreate(ID3D12Device& device, key_t arg_shapes);
+
+//    /// destroys all elements inside, and clears the map
+//    void reset();
+
+//private:
+//    std::unordered_map<key_t, root_signature_ll, hash::compute_functor> mCache;
+//};
 
 }
