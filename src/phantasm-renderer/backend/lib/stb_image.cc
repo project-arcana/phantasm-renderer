0ffa34ab91ebdbcfa4b89db234512e23f3fc7ee8
Jonathan Kunstwald <jonathan.kunstwald@rwth-aachen.de>
2019-12-08 19:51:10 +0100

    Move image loader and mesh loader to incubator

--- src/phantasm-renderer/backend/lib/stb_image.cc
+++
@@ -1,6 +0,0 @@
-#include <clean-core/assert.hh>
-
-#define STBI_ASSERT(_arg_) CC_ASSERT(_arg_)
-
-#define STB_IMAGE_IMPLEMENTATION
-#include "stb_image.hh"
--- src/phantasm-renderer/backend/vulkan/memory/ResourceAllocator.hh
+++ src/phantasm-renderer/backend/vulkan/memory/ResourceAllocator.hh
@@ -5,44 +5,13 @@
 typedef struct VmaAllocator_T* VmaAllocator;
 typedef struct VmaAllocation_T* VmaAllocation;
 
-namespace pr::backend::assets
-{
-struct image_size;
-};
-
 namespace pr::backend::vk
 {
-struct buffer
-{
-    VkBuffer buffer;
-    VmaAllocation allocation;
-};
-
-struct image
-{
-    VkImage image;
-    VmaAllocation allocation;
-};
-
 class ResourceAllocator
 {
 public:
     void initialize(VkPhysicalDevice physical, VkDevice device);
     void destroy();
-
-    [[nodiscard]] buffer allocBuffer(uint32_t size, VkBufferUsageFlags usage);
-
-    [[nodiscard]] buffer allocCPUtoGPUBuffer(uint32_t size, VkBufferUsageFlags usage, void** map_ptr);
-
-    [[nodiscard]] image allocAssetTexture(assets::image_size const& image_size, bool use_srgb = false);
-
-    [[nodiscard]] image allocRenderTarget(unsigned width, unsigned height, VkFormat format, VkImageUsageFlags usage);
-
-
-    void unmapCPUtoGPUBuffer(buffer const& buffer);
-
-    void free(buffer const& buffer);
-    void free(image const& image);
 
     [[nodiscard]] VmaAllocator getAllocator() const { return mAllocator; }
 
