59a9e547ae74e758a6f75d2ef06d1d1ac1c94776
Jonathan Kunstwald <jonathan.kunstwald@rwth-aachen.de>
2020-04-04 19:05:02 +0200

    Add comment

--- src/phantasm-renderer/backends.hh
+++ src/phantasm-renderer/backends.hh
@@ -4,6 +4,7 @@
 
 namespace pr
 {
+// TODO internal usage only
 [[nodiscard]] phi::Backend* make_vulkan_backend(phi::window_handle const& window_handle, phi::backend_config const& cfg);
 [[nodiscard]] phi::Backend* make_d3d12_backend(phi::window_handle const& window_handle, phi::backend_config const& cfg);
 }
