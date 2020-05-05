b286c33378df22d4713291d573338db2cdfcd26c
Jonathan Kunstwald <jonathan.kunstwald@rwth-aachen.de>
2020-05-05 19:31:55 +0200

    Use phi::arg resource info

--- src/phantasm-renderer/common/resource_info.hh
+++ src/phantasm-renderer/common/resource_info.hh
@@ -2,53 +2,13 @@
 
 #include <clean-core/hash.hh>
 
-#include <phantasm-hardware-interface/types.hh>
+#include <phantasm-hardware-interface/arguments.hh>
 
 namespace pr
 {
-struct render_target_info
-{
-    phi::format format;
-    int width;
-    int height;
-    unsigned num_samples;
-    unsigned array_size;
-
-    constexpr bool operator==(render_target_info const& rhs) const noexcept
-    {
-        return format == rhs.format && width == rhs.width && height == rhs.height && num_samples == rhs.num_samples && array_size == rhs.array_size;
-    }
-};
-
-struct texture_info
-{
-    phi::format fmt;
-    phi::texture_dimension dim;
-    bool allow_uav;
-    int width;
-    int height;
-    unsigned depth_or_array_size;
-    unsigned num_mips;
-
-    constexpr bool operator==(texture_info const& rhs) const noexcept
-    {
-        return fmt == rhs.fmt && dim == rhs.dim && allow_uav == rhs.allow_uav && width == rhs.width && height == rhs.height
-               && depth_or_array_size == rhs.depth_or_array_size && num_mips == rhs.num_mips;
-    }
-};
-
-struct buffer_info
-{
-    unsigned size_bytes;
-    unsigned stride_bytes;
-    bool allow_uav;
-    phi::resource_heap heap;
-
-    constexpr bool operator==(buffer_info const& rhs) const noexcept
-    {
-        return size_bytes == rhs.size_bytes && stride_bytes == rhs.stride_bytes && allow_uav == rhs.allow_uav && heap == rhs.heap;
-    }
-};
+using render_target_info = phi::arg::create_render_target_info;
+using texture_info = phi::arg::create_texture_info;
+using buffer_info = phi::arg::create_buffer_info;
 
 struct resource_info_hasher
 {
