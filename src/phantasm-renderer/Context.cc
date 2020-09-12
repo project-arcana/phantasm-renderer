15974db43f6e70ec4bd139ffcf963c265a5e360e
Jonathan Kunstwald <jonathan.kunstwald@rwth-aachen.de>
2020-09-12 19:43:47 +0200

    Adapt to dxcw change

--- src/phantasm-renderer/Context.cc
+++ src/phantasm-renderer/Context.cc
@@ -144,7 +144,7 @@ auto_shader_binary Context::make_shader(cc::string_view code, cc::string_view en
 
     {
         auto lg = std::lock_guard(mMutexShaderCompilation); // unsynced, mutex: compilation
-        bin = mShaderCompiler.compile_binary(code.data(), entrypoint.data(), sc_target, sc_output);
+        bin = mShaderCompiler.compile_shader(code.data(), entrypoint.data(), sc_target, sc_output);
     }
 
     if (bin.data == nullptr)
