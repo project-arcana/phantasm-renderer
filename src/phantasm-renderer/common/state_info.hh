89e033c0c4068511a5a4dea4f3d65caec39c7ca1
Jonathan Kunstwald <jonathan.kunstwald@rwth-aachen.de>
2020-09-16 20:46:04 +0200

    Phi changes

--- src/phantasm-renderer/common/state_info.hh
+++ src/phantasm-renderer/common/state_info.hh
@@ -3,7 +3,7 @@
 #include <clean-core/typedefs.hh>
 
 #include <phantasm-hardware-interface/arguments.hh>
-#include <phantasm-hardware-interface/common/container/trivial_capped_vector.hh>
+#include <phantasm-hardware-interface/common/container/flat_vector.hh>
 #include <phantasm-hardware-interface/types.hh>
 
 
@@ -26,11 +26,11 @@ struct freeable_cached_obj
 // for economic reasons, SRVs, UAVs and Samplers are limited for cache-access shader views
 struct shader_view_info
 {
-    phi::detail::trivial_capped_vector<phi::resource_view, 4> srvs;
-    phi::detail::trivial_capped_vector<phi::resource_view, 4> uavs;
-    phi::detail::trivial_capped_vector<uint64_t, 4> srv_guids;
-    phi::detail::trivial_capped_vector<uint64_t, 4> uav_guids;
-    phi::detail::trivial_capped_vector<phi::sampler_config, 2> samplers;
+    phi::flat_vector<phi::resource_view, 4> srvs;
+    phi::flat_vector<phi::resource_view, 4> uavs;
+    phi::flat_vector<uint64_t, 4> srv_guids;
+    phi::flat_vector<uint64_t, 4> uav_guids;
+    phi::flat_vector<phi::sampler_config, 2> samplers;
 };
 
 struct graphics_pass_info_data
@@ -38,15 +38,15 @@ struct graphics_pass_info_data
     phi::pipeline_config graphics_config = {};
     unsigned vertex_size_bytes = 0;
     bool has_root_consts = false;
-    phi::detail::trivial_capped_vector<phi::vertex_attribute_info, 8> vertex_attributes;
-    phi::detail::trivial_capped_vector<phi::arg::shader_arg_shape, phi::limits::max_shader_arguments> arg_shapes;
-    phi::detail::trivial_capped_vector<cc::hash_t, phi::limits::num_graphics_shader_stages> shader_hashes;
+    phi::flat_vector<phi::vertex_attribute_info, 8> vertex_attributes;
+    phi::flat_vector<phi::arg::shader_arg_shape, phi::limits::max_shader_arguments> arg_shapes;
+    phi::flat_vector<cc::hash_t, phi::limits::num_graphics_shader_stages> shader_hashes;
 };
 
 struct compute_pass_info_data
 {
     bool has_root_consts = false;
-    phi::detail::trivial_capped_vector<phi::arg::shader_arg_shape, phi::limits::max_shader_arguments> arg_shapes;
+    phi::flat_vector<phi::arg::shader_arg_shape, phi::limits::max_shader_arguments> arg_shapes;
     cc::hash_t shader_hash;
 };
 }
