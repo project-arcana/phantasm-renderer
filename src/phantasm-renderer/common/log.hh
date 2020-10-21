6729b9ea4176e30de1abd496dbb7fc7fa4f8b556
Philip Trettner <Philip.Trettner@rwth-aachen.de>
2020-10-21 17:29:19 +0200

    warning fixes, adapt rlog changes

--- src/phantasm-renderer/common/log.hh
+++ src/phantasm-renderer/common/log.hh
@@ -7,20 +7,20 @@ namespace pr::detail
 static constexpr rlog::domain domain = rlog::domain("PR");
 static constexpr rlog::severity assert_severity = rlog::severity("ASSERT", "\u001b[38;5;196m\u001b[1m");
 
-constexpr void info_log(rlog::MessageBuilder& builder) { builder.set_domain(domain); }
-constexpr void warn_log(rlog::MessageBuilder& builder)
+inline void info_log(rlog::MessageBuilder& builder) { builder.set_domain(domain); }
+inline void warn_log(rlog::MessageBuilder& builder)
 {
     builder.set_domain(domain);
     builder.set_severity(rlog::severity::warning());
     builder.set_use_error_stream(true);
 }
-constexpr void err_log(rlog::MessageBuilder& builder)
+inline void err_log(rlog::MessageBuilder& builder)
 {
     builder.set_domain(domain);
     builder.set_severity(rlog::severity::error());
     builder.set_use_error_stream(true);
 }
-constexpr void assert_log(rlog::MessageBuilder& builder)
+inline void assert_log(rlog::MessageBuilder& builder)
 {
     builder.set_domain(domain);
     builder.set_severity(assert_severity);
