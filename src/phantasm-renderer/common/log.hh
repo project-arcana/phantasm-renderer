08e5cb7fc2a90bf5110a7bebfe08bed249e830ec
Jonathan Kunstwald <kunstwald@shapedcode.com>
2023-02-22 18:40:11 +0100

    Fix compatibility with new rlog

--- src/phantasm-renderer/common/log.hh
+++ src/phantasm-renderer/common/log.hh
@@ -1,29 +1,7 @@
 #pragma once
 
-#include <rich-log/detail/log_impl.hh>
+#include <phantasm-hardware-interface/common/log.hh>
 
-namespace pr::detail
-{
-static constexpr rlog::domain domain = rlog::domain("PR");
-
-inline void info_log(rlog::MessageBuilder& builder) { builder.set_domain(domain); }
-inline void warn_log(rlog::MessageBuilder& builder)
-{
-    builder.set_domain(domain);
-    builder.set_severity(rlog::severity::warning);
-}
-inline void err_log(rlog::MessageBuilder& builder)
-{
-    builder.set_domain(domain);
-    builder.set_severity(rlog::severity::error);
-}
-inline void assert_log(rlog::MessageBuilder& builder)
-{
-    builder.set_domain(domain);
-    builder.set_severity(rlog::severity::critical);
-}
-}
-
-#define PR_LOG RICH_LOG_IMPL(pr::detail::info_log)
-#define PR_LOG_WARN RICH_LOG_IMPL(pr::detail::warn_log)
-#define PR_LOG_ERROR RICH_LOG_IMPL(pr::detail::err_log)
+#define PR_LOG PHI_LOG
+#define PR_LOG_WARN PHI_LOG_WARN
+#define PR_LOG_ERROR PHI_LOG_ERROR
